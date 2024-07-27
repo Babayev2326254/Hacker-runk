ef8f1114b45a1be883854dd7a788e88a7d270d37
Babayev2326254 <f.babayev1389@gmail.com>
2024-07-27 11:31:17 +0400

    hacker runh homevork 345.py homework.py attached Eldar Teacher

---
+++ hacker runh homevork 345.py
@@ -0,0 +1,20 @@
+#tapşırıq 3 
+a = int(input())
+b = int(input())
+if 1 <= a <= 10**10 and 1 <= b <= 10**10 :
+    print(str(a + b))
+    print(str(a-b))
+    print(str(a*b))
+
+#tapsiriq 4
+a = int(input())
+b = int(input())
+print(str(a//b))
+print(str(a/b))
+
+#tapsiriq 5
+n = int(input())
+if 1<= n <= 20 :
+    for i in range(0,n):
+        i =i**2
+        print(i)
