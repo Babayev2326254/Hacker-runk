a5ed992e11bbc10dea8db3b406c13f39fea5b6b5
Babayev2326254 <f.babayev1389@gmail.com>
2024-08-02 15:29:39 +0400

    Update hacker runh homevork 345.py

--- hacker runh homevork 345.py
+++ hacker runh homevork 345.py
@@ -1,20 +1,31 @@
-#tapşırıq 3 
-a = int(input())
-b = int(input())
-if 1 <= a <= 10**10 and 1 <= b <= 10**10 :
-    print(str(a + b))
-    print(str(a-b))
-    print(str(a*b))
-
-#tapsiriq 4
-a = int(input())
-b = int(input())
-print(str(a//b))
-print(str(a/b))
-
-#tapsiriq 5
+#https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
+n = int(input())
+if  n//2 == float(n/2) :
+    if 2<n<5 :
+        print("Not Weird")
+    elif 6<n<=20 :
+        print("Weird")
+    elif n>20 :
+        print("Not Weird")
+else :
+    print("Weird")
+#https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
 n = int(input())
 if 1<= n <= 20 :
     for i in range(0,n):
         i =i**2
         print(i)
+#https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+a = int(input())
+b = int(input())
+print(str(a//b))
+print(str(a/b))
+
+
+#https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
+n = int(input())
+if 1 <= n <= 150:
+    for a in range (1,n+1):
+        print(a,end='')
+else:
+    print(n)
